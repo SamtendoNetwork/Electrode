9f1879b50e0e971f23bebea3015865b835270963
3d huh? <145678809+Whatsauser@users.noreply.github.com>
2026-08-21 18:25:24 +0300

    add olv (Olive/Miiverse) urls into dns_hook

--- src/patches/dns_hooks.cpp
+++ src/patches/dns_hooks.cpp
@@ -30,6 +30,8 @@ constexpr std::pair<const char *, const char *> dns_replacements[] = {
         // NNCS servers
         { "nncs1.app.nintendowifi.net", "nncs1.app." NETWORK_BASEURL },
         { "nncs2.app.nintendowifi.net", "nncs2.app." NETWORK_BASEURL },
+        { "discovery.olv.nintendo.net", "discovery-olv.samtendo.net" },
+        { "api.olv.nintendo.net", "api-olv.samtendo.net" },
 };
 
 static const char * replace_dns_name(const char *dns_name) {
