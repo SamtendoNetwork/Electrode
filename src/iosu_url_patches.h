851b2db02ea6ad0936d6b385aba5cad5b07b2770
Sam <rdp@samtendo.net>
2026-09-20 18:05:26 +0100

    Add BOSS URLs

--- src/iosu_url_patches.h
+++ src/iosu_url_patches.h
@@ -18,12 +18,12 @@ static const URL_Patch url_patches[] = {
         {0xE2282DB4, "https://ecs.wup.shop.pretendo.cc/ecs/services/ECommerceSOAP"},
         {0xE22830A0, "https://ecs.wup.shop.pretendo.cc/ecs/services/ECommerceSOAP"},
         {0xE22830E0, "https://nus.wup.shop.pretendo.cc/nus/services/NetUpdateSOAP"},
-        {0xE2299990, "nppl.app.pretendo.cc"},
+        {0xE2299990, "nppl.app.samtendo.net"},
         {0xE229A600, "https://pls.wup.shop.pretendo.cc/pls/upload"},
         {0xE229A6AC, "https://npvk-dev.app.pretendo.cc/reports"},
         {0xE229A6D8, "https://npvk.app.pretendo.cc/reports"},
-        {0xE229B1F4, "https://npts.app.pretendo.cc/p01/tasksheet/%s/%s/%s/%s?c=%s&l=%s"},
-        {0xE229B238, "https://npts.app.pretendo.cc/p01/tasksheet/%s/%s/%s?c=%s&l=%s"},
+        {0xE229B1F4, "https://npts.app.samtendo.net/p01/tasksheet/%s/%s/%s/%s?c=%s&l=%s"},
+        {0xE229B238, "https://npts.app.samtendo.net/p01/tasksheet/%s/%s/%s?c=%s&l=%s"},
         {0xE22AB2D8, "https://idbe-wup.cdn.pretendo.cc/icondata/%02X/%016llX.idbe"},
         {0xE22AB318, "https://idbe-ctr.cdn.pretendo.cc/icondata/%02X/%016llX.idbe"},
         {0xE22AB358, "https://idbe-wup.cdn.pretendo.cc/icondata/%02X/%016llX-%d.idbe"},
@@ -35,7 +35,7 @@ static const URL_Patch url_patches[] = {
         {0xE22B3FFC, "https://nus.c.shop.pretendo.cc/nus/services/NetUpdateSOAP"},
         {0xE229DE0C, "n.app.pretendo.cc"},
         //nim-boss .bss
-        {0xE24B8A24, "https://nppl.app.pretendo.cc/p01/policylist/1/1/UNK"},
+        {0xE24B8A24, "https://nppl.app.samtendo.net/p01/policylist/1/1/UNK"},
         {0xE31930D4, "https://%s%saccount.samtendo.net/v%u/api/"}
 
 };
